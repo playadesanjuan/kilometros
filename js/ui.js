13abdfdb3650db6569d839f960b773f5d3baa252
playadesanjuan <166201403+playadesanjuan@users.noreply.github.com>
2026-06-28 17:05:01 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,21 @@
+const UI = {
 
+    mostrarMensaje(texto, tipo = "ok") {
+
+        const mensaje = document.getElementById("mensaje");
+
+        mensaje.innerHTML = texto;
+
+        mensaje.className = "mensaje " + tipo;
+
+        setTimeout(() => {
+
+            mensaje.innerHTML = "";
+
+            mensaje.className = "mensaje";
+
+        }, 3000);
+
+    }
+
+};
