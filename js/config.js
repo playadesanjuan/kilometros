e15392c295de0a098a95ff3c36f6e882b8021bc9
playadesanjuan <166201403+playadesanjuan@users.noreply.github.com>
2026-06-28 17:11:16 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@ const Config = {
 
     VERSION: "1.0.0",
 
-    API_URL: "https://script.google.com/macros/s/TU_SCRIPT/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbzp_2bfW2kT9GjOqrx3XYtieyGUXoi9FzP-We8NEdJnEs1KkToKtThmpKCxp6-xXb1T/exec"
 
 };
