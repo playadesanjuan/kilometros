b9f9d2c2f9022570d3a32272efab3637878b9ee8
playadesanjuan <166201403+playadesanjuan@users.noreply.github.com>
2026-06-28 17:17:51 +0200

    Update navegacion.js

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,1 +1,43 @@
+const Navegacion = {
 
+    iniciar() {
+
+        document
+            .getElementById("menuInicio")
+            .onclick = () => this.ir("Inicio");
+
+        document
+            .getElementById("menuHistorial")
+            .onclick = () => this.ir("Historial");
+
+        document
+            .getElementById("menuEstadisticas")
+            .onclick = () => this.ir("Estadisticas");
+
+        document
+            .getElementById("menuPVPC")
+            .onclick = () => this.ir("PVPC");
+
+    },
+
+    ir(nombre) {
+
+        document
+            .querySelectorAll(".pantalla")
+            .forEach(p=>p.classList.remove("activa"));
+
+        document
+            .querySelectorAll("nav a")
+            .forEach(p=>p.classList.remove("active"));
+
+        document
+            .getElementById("pantalla"+nombre)
+            .classList.add("activa");
+
+        document
+            .getElementById("menu"+nombre)
+            .classList.add("active");
+
+    }
+
+}
